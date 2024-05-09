bc63845f2bc96cd3609e0475063b4016bfd68a8c
paulo-junior-oficialfarma <paulo.junior@oficialfarma.com.br>
2024-05-09 16:57:46 -0300

    feat - docusaurus config edit link update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,7 +29,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          "https://github.com/paulo-junior-oficialfarma/oficial_docs/tree/master/",
           docLayoutComponent: "@theme/DocPage",
           docItemComponent: "@theme/ApiItem", // Derived from docusaurus-theme-openapi
         },
